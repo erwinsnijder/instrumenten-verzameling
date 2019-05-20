3227d6fe744001e280a4a3dcd93ffc4dc1d280cf
erwin snijder <erwinsnijder_4@hotmail.com>
2019-05-20 11:11:16 +0200

    Update html.js

--- html.js
+++ html.js
@@ -10,13 +10,16 @@ let gitaar2 = new gitaar('Davidson', 'zwart');
 
 class drumstel {
   constructor(merk, kleur) {
-    this.merk = kleur;
-    this.kleur = merk;
+    this.merk = merk;
+    this.kleur = kleur;
    }
 }
 
+let drumstel1 = new drumstel ('bob', 'groen');
+let drumstel2 = new drumstel('marley', 'geel');
+
 let instrumenten = [];
 
-instrumenten.push(gitaar1, gitaar2);
+instrumenten.push(gitaar1, gitaar2, drumstel1, drumstel2);
 
 console.log(instrumenten)
