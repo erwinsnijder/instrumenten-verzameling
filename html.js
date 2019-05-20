835e42601588959dfee269db935e49742d4b99df
erwin snijder <erwinsnijder_4@hotmail.com>
2019-05-20 10:29:21 +0200

    Initial commit

---
+++ html.js
@@ -0,0 +1,40 @@
+// class details {
+//   constructor(merk, kleur) {
+//     this.merk = merk;
+//     this.kleur = kleur;
+//   }
+// }
+
+  class gitaar {
+    constructor(harley, rood) {
+    this.merk = harley;
+    this.kleur = rood;
+      //   this.details =[]
+      
+    // this.details.push(new details);  
+
+    }
+}
+
+class drumstel {
+  constructor(thompson, roze) {
+    this.merk = thompson;
+    this.kleur = roze;
+    // this.fabrikant = harley;
+    // this.kleur = rood;
+    
+
+  }
+}
+
+this.instrumenten = [];
+
+this.instrumenten.push(new gitaar);
+this.instrumenten.push(new drumstel);
+
+
+// function myFunction() {
+//     for (var x = 0; x < instrumenten.length; x++) {  
+//       document.getElementById("verzameling").innerHTML = instrumentten; }}
+
+   console.log(instrumenten)
